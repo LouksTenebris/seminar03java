a2793b6a37431c59c16cd8bd4931ee387ae2dc6e
Louks <121496015+LouksTenebris@users.noreply.github.com>
2023-06-21 13:32:15 +0300

    End

---
+++ src/main/java/org/example/task3/Books.java
@@ -0,0 +1,19 @@
+package org.example.task3;
+
+import louks.AllArgsConstructor;
+import louks.Data;
+
+@Data
+@AllArgsConstructor
+public class Books {
+
+    private String name;
+    private String author;
+    private Double price;
+    private Integer year;
+    private Integer pgNum;
+
+
+
+}
+
